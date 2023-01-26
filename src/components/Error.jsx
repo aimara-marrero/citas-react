477d59cc89a442eb784922e4843f12550cf72c85
aimara-marrero <aimara.bmarrero@gmail.com>
2023-01-26 08:40:20 +0000

    ERROR BG-RED-800

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({children}) => {
   return (
-    <div className="bg-red-700 text-center uppercase text-white font-bold rounded-md p-3 mb-3">
+    <div className="bg-red-800 text-center uppercase text-white font-bold rounded-md p-3 mb-3">
      
      {children}
     </div>
